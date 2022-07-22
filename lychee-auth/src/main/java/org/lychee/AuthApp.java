27beacb4f12adaa0852d72697b86129d0f1dc63f
lqscr88 <62920348+lqscr88@users.noreply.github.com>
2022-07-22 14:13:17 +0800

    auth调整

--- lychee-auth/src/main/java/org/lychee/AuthApp.java
+++ lychee-auth/src/main/java/org/lychee/AuthApp.java
@@ -1,5 +1,7 @@
 package org.lychee;
 
+import org.lychee.config.LycheeApplication;
+import org.lychee.constant.AppConstant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,7 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class AuthApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthApp.class, args);
+        LycheeApplication.run(AppConstant.APPLICATION_AUTH_NAME, AuthApp.class, args);
     }
 
 }
--- lychee-gateway/src/main/java/org/lychee/GatewayApp.java
+++ lychee-gateway/src/main/java/org/lychee/GatewayApp.java
@@ -4,12 +4,10 @@ package org.lychee;
 import org.lychee.config.LycheeApplication;
 import org.lychee.constant.AppConstant;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableCaching
 public class GatewayApp {
 
 
