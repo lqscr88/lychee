0e0f1afc0614a70c71af5920237f1d8032762fa9
lqscr88 <62920348+lqscr88@users.noreply.github.com>
2022-08-02 14:22:41 +0800

    结构调整

--- lychee-auth/src/main/java/org/lychee/config/WebSecurityConfig.java
+++ lychee-auth/src/main/java/org/lychee/config/WebSecurityConfig.java
@@ -1,39 +1,85 @@
 package org.lychee.config;
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService sysUserDetailsService;
+    private final StringRedisTemplate redisTemplate;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-         http
-            .authorizeRequests().requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
-        .and()
-            .authorizeRequests().antMatchers("/getPublicKey").permitAll().anyRequest().authenticated()
-        .and()
-            .csrf().disable();
+        http
+                .authorizeRequests().antMatchers("/oauth/**", "/sms-code").permitAll()
+                // @link https://gitee.com/xiaoym/knife4j/issues/I1Q5X6 (接口文档knife4j需要放行的规则)
+                .antMatchers("/webjars/**", "/doc.html", "/swagger-resources/**", "/v2/api-docs").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .csrf().disable();
     }
 
     /**
-     *  如果不配置SpringBoot会自动配置一个AuthenticationManager,覆盖掉内存中的用户
+     * 认证管理对象
+     *
+     * @return
+     * @throws Exception
      */
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) {
+        auth.
+                authenticationProvider(daoAuthenticationProvider());
+    }
+
+
+
+    /**
+     * 用户名密码认证授权提供者
+     *
+     * @return
+     */
     @Bean
-    public PasswordEncoder passwordEncoder()  {
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(sysUserDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setHideUserNotFoundExceptions(false); // 是否隐藏用户不存在异常，默认:true-隐藏；false-抛出异常；
+        return provider;
+    }
+
+
+    /**
+     * 密码编码器
+     * <p>
+     * 委托方式，根据密码的前缀选择对应的encoder，例如：{bcypt}前缀->标识BCYPT算法加密；{noop}->标识不使用任何加密即明文的方式
+     * 密码判读 DaoAuthenticationProvider#additionalAuthenticationChecks
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
+
+
 }
